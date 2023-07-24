2334a508237c4d75b85075402348faf4f198ce42
rodgersmm <rodgersmutua44@gmail.com>
2023-07-24 16:14:49 +0300

    implements DatabaseManagement

---
+++ src/main/java/Sighting.java
@@ -0,0 +1,10 @@
+
+
+
+public class Sighting implements DatabaseManagement {
+
+    private int id;
+    private int animal_id;
+    private String location;
+    private String ranger_name;
+    private Timestamp timestamp;
