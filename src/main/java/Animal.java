d8c50f344f847af11832f94357444f79a1196d5c
rodgersmm <rodgersmutua44@gmail.com>
2023-07-24 16:00:03 +0300

    Set Animal Name

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,7 +1,45 @@
 
 
 public class Animal {
-    
+    public int id;
+    public String name;
+    public String type;
+
+
+    //Create non endangered animal constant
+    public static final String ANIMAL_TYPE = "Non-endangered";
+
+    public Animal(String name){
+        if (name.equals("")){
+            //throw exception if no name is entered
+            throw new IllegalArgumentException("Please enter an animal name.");
+        }
+        this.name = name;
+
+        type = ANIMAL_TYPE;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    //Set Animal Name
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
 
     public static void main(String[] args) {
         //
